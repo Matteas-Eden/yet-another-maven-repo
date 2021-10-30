344a86b7e48b462f84e3dbd420113c5882dd28ad
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2021-10-30 18:29:25 +1300

    [Feature] Increase Test Coverage (#1)
    * Uncomment tests
    
    * Remove unnecessary public modifiers
    
    * Update generate-badge to succeed on failed push
    
    The most likely fail-case for being unable to push to master is that
    there are no changes to the badge SVGs, and that is not enough of
    a reason to fail the workflow.

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -25,12 +25,12 @@ public class PersonTest {
         assertEquals(25, person.getAge());
     }
 
-//    @Test
-//    void personHasBirthdayAndGetsOlder() {
-//        Person person = new Person("John", "Smith", 25, Color.BLUE, Color.BLACK);
-//        person.haveBirthday();
-//        assertEquals(26, person.getAge());
-//    }
+    @Test
+    void personHasBirthdayAndGetsOlder() {
+        Person person = new Person("John", "Smith", 25, Color.BLUE, Color.BLACK);
+        person.haveBirthday();
+        assertEquals(26, person.getAge());
+    }
 
     @Test
     void personHasEyeColour() {
@@ -38,14 +38,14 @@ public class PersonTest {
         assertEquals(Color.BLUE, person.getEyeColour());
     }
 
-//    @Test
-//    void personHasHairColour() {
-//        Person person = new Person("John", "Smith", 25, Color.BLUE, Color.BLACK);
-//        assertEquals(Color.BLACK, person.getHairColour());
-//    }
+    @Test
+    void personHasHairColour() {
+        Person person = new Person("John", "Smith", 25, Color.BLUE, Color.BLACK);
+        assertEquals(Color.BLACK, person.getHairColour());
+    }
 
     @Test
-    public void personDyesHair() {
+    void personDyesHair() {
         Person person = new Person("John", "Smith", 25, Color.BLUE, Color.BLACK);
         person.dyeHair(Color.RED);
         assertEquals(Color.RED, person.getHairColour());
