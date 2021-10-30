ddd59d7572d468346f1a9eaac204878f40abc9da
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2021-10-30 16:42:19 +1300

    Increase test coverage
    Also removes some code smells regarding the 'public' modifier in tests

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -8,38 +8,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PersonTest {
 
     @Test
-    public void personHasFirstName() {
+    void personHasFirstName() {
         Person person = new Person("John", "Smith", 25, Color.BLUE, Color.BLACK);
         assertEquals("John", person.getFirstName());
     }
 
     @Test
-    public void personHasLastName() {
+    void personHasLastName() {
         Person person = new Person("John", "Smith", 25, Color.BLUE, Color.BLACK);
         assertEquals("Smith", person.getLastName());
     }
 
     @Test
-    public void personHasAge() {
+    void personHasAge() {
         Person person = new Person("John", "Smith", 25, Color.BLUE, Color.BLACK);
         assertEquals(25, person.getAge());
     }
 
     @Test
-    public void personHasBirthdayAndGetsOlder() {
+    void personHasBirthdayAndGetsOlder() {
         Person person = new Person("John", "Smith", 25, Color.BLUE, Color.BLACK);
         person.haveBirthday();
         assertEquals(26, person.getAge());
     }
 
+    @Test
+    void personHasEyeColour() {
+        Person person = new Person("John", "Smith", 25, Color.BLUE, Color.BLACK);
+        assertEquals(Color.BLUE, person.getEyeColour());
+    }
+
 //    @Test
-//    public void personHasEyeColour() {
-//        Person person = new Person("John", "Smith", 25, Color.BLUE, Color.BLACK);
-//        assertEquals(Color.BLUE, person.getEyeColour());
-//    }
-//
-//    @Test
-//    public void personHasHairColour() {
+//    void personHasHairColour() {
 //        Person person = new Person("John", "Smith", 25, Color.BLUE, Color.BLACK);
 //        assertEquals(Color.BLACK, person.getHairColour());
 //    }
