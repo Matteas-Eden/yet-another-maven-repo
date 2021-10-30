acafbfddf18cbd189148c6a66b4334bf560f6342
Ernest Wong <ewon521@gmail.com>
2021-10-30 19:31:32 +1300

    [Feature] Add `Student` class
    Add `Student` class is added.
    Basic tracking of test scores is implemented.
    
    Risk areas:
    - None, as new Student class is not used.

---
+++ src/main/java/Student.java
@@ -0,0 +1,11 @@
+import java.swing.text.html.HTMLDocument;
+
+public class Student extends Person {
+    public final HTMLDocument testScore;
+
+    public Student(String firstName, String lastName, int age, Color eyeColour, Color hairColour, HTMLDocument testScore) {
+        super(firstName, lastName, age, eyeColour, hairColour);
+        this.testScore = testScore;
+    }
+}
+
