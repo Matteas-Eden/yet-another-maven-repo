72e9c99ffb5175d97cb009234f9ff76d4006b9e2
Ernest Wong <ewon521@gmail.com>
2021-10-31 19:28:31 +1300

    Add getter for testScore

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,11 +2,15 @@ import javax.swing.text.html.HTMLDocument;
 import java.awt.*;
 
 public class Student extends Person {
-    public final HTMLDocument testScore;
+    private final HTMLDocument testScore;
 
     public Student(String firstName, String lastName, int age, Color eyeColour, Color hairColour, HTMLDocument testScore) {
         super(firstName, lastName, age, eyeColour, hairColour);
         this.testScore = testScore;
+    }
+
+    public HTMLDocument getTestScore() {
+        return this.testScore;
     }
 }
 
