f77b349abfacc1ca40f56530902010a241d5c84e
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2021-11-03 21:44:42 +1300

    Resolve issues highlighted by CodeClimate (#10)

--- src/main/java/io/github/matteas/Student.java
+++ src/main/java/io/github/matteas/Student.java
@@ -1,12 +1,13 @@
 package io.github.matteas;
 
+import java.awt.Color;
 import javax.swing.text.html.HTMLDocument;
-import java.awt.*;
 
 public class Student extends Person {
     private final HTMLDocument testScore;
 
-    public Student(String firstName, String lastName, int age, Color eyeColour, Color hairColour, HTMLDocument testScore) {
+    public Student(String firstName, String lastName, int age, Color eyeColour,
+                   Color hairColour, HTMLDocument testScore) {
         super(firstName, lastName, age, eyeColour, hairColour);
         this.testScore = testScore;
     }
