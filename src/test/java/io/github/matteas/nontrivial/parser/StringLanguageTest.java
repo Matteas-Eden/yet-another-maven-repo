c3fc207490d9806ecb25d6600c99b88377470813
Ernest Wong <ewon521@gmail.com>
2022-05-27 13:13:04 +0000

    Fix out of date import for CharacterIterator

--- src/test/java/io/github/matteas/nontrivial/parser/StringLanguageTest.java
+++ src/test/java/io/github/matteas/nontrivial/parser/StringLanguageTest.java
@@ -3,7 +3,7 @@ package io.github.matteas.nontrivial.parser;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.List;
-import io.github.matteas.nontrivial.lexer.CharacterIterator;
+import io.github.matteas.nontrivial.util.CharacterIterator;
 
 class StringLanguageTest {
     @Test
