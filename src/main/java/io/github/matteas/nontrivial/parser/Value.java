2cb02480766adabb0eb78c99b4b40aa4b4d96cd1
Ernest Wong <ewon521@gmail.com>
2022-05-27 07:41:06 +0000

    Revert nonnull annotation at it doesn't fix the previous error in Syntax.Sequence

--- src/main/java/io/github/matteas/nontrivial/parser/Value.java
+++ src/main/java/io/github/matteas/nontrivial/parser/Value.java
@@ -1,7 +1,5 @@
 package io.github.matteas.nontrivial.parser;
 
-import org.checkerframework.checker.nullness.qual.NonNull;
-
-public interface Value<V extends @NonNull Value<V>> {
+public interface Value<V extends Value<V>> {
     V prepend(V value);
 }
